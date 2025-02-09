c576cb739cc3566b7ce6f8ac514e7a2134419128
MattSouza14 <mateussouzabr1@gmail.com>
2025-02-08 21:11:03 -0300

    Atualização no header, adição da lovo nova SVG, remoção do navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,21 +4,4 @@ import "controllers"
 import "trix"
 import "@rails/actiontext"
 
-document.addEventListener("turbo:load", function () {
-    const toggleButton = document.querySelector("[data-collapse-toggle]");
-    const navbarHamburger = document.getElementById("navbar-hamburger");
-  
-    if (toggleButton && navbarHamburger) {
-      toggleButton.addEventListener("click", function () {
-        const isExpanded = toggleButton.getAttribute("aria-expanded") === "true";
-  
-        if (isExpanded) {
-          navbarHamburger.classList.add("hidden");
-          toggleButton.setAttribute("aria-expanded", "false");
-        } else {
-          navbarHamburger.classList.remove("hidden");
-          toggleButton.setAttribute("aria-expanded", "true");
-        }
-      });
-    }
-});
+
