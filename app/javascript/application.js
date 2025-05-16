237b46bca9e76a22f1b597d9d2fd2549d8282e2f
Jmspter <jamesideias@gmail.com>
2025-05-16 11:44:13 -0300

    Views de articles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -168,3 +168,27 @@ async function renderPDF(url, containerId) {
 
 // Exporta a função se quiser usar em outros scripts
 window.renderPDF = renderPDF;
+
+
+let deferredPrompt;
+const installButton = document.getElementById('installApp');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installButton.hidden = false;
+});
+
+installButton.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === 'accepted') {
+      console.log('Usuário aceitou a instalação');
+    } else {
+      console.log('Usuário recusou a instalação');
+    }
+    deferredPrompt = null;
+    installButton.hidden = true;
+  }
+});
